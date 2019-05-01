c71a0b89505606d9e006d4ce31d738e3a8cb70a3
Sherryairui <43098229+Sherryairui@users.noreply.github.com>
2019-05-01 15:26:46 -0400

    Add files via upload

--- bdm/final_xc1454.py
+++ bdm/final_xc1454.py
@@ -96,12 +96,13 @@ def toCSVLine(data):
 if __name__ == "__main__": 
 
 	sc = SparkContext()
-	rdd = sc.textFile('hdfs:///tmp/bdm/tweets-100m.csv')
+	rdd = sc.textFile('tweets.csv')
 	counts = rdd.mapPartitionsWithIndex(processDrugs) \
 	            .reduceByKey(lambda x,y: x+y) \
 	            .map(lambda x: (x[0][0], float(x[1])/x[0][1])) \
                 .sortByKey(ascending=True).map(toCSVLine) \
-                .coalesce(1, True).saveAsTextFile('geo_ratio')
+                .coalesce(1, True).write.format('com.databricks.spark.csv') \
+                .options(header='true').saveAsTextFile('geo_ratio')
 
     #lines = labelsAndPredictions.map(toCSVLine)
     #counts.saveAsTextFile('/home/xc1454/file_transfer/bdm/geo_ratio.csv')
